26b497121a08cc2b8b70b6c9be5839ae359032d8
Masao Kitamura <masaok@gmail.com>
2022-02-02 23:12:55 -0800

    Adding React Router DOM and World component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import logo from './logo.svg'
 import './App.css'
 
+import { Routes, Route, BrowserRouter as Router } from 'react-router-dom'
+
 import Hello from './components/Hello'
+import World from './components/World'
 
 function App() {
   return (
@@ -9,6 +12,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Hello />
+        <Router>
+          <Routes>
+            <Route path="/world" element={<World />} />
+          </Routes>
+        </Router>
       </header>
     </div>
   )
---
+++ src/components/World.jsx
@@ -0,0 +1,5 @@
+const World = (props) => {
+  return <div>WORLD</div>
+}
+
+export default World
